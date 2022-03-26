646068db1c03b052f62db9ea297dc13e8720dec3
Julian Gonzalez <julianmarcos505@gmail.com>
2022-03-25 21:21:08 -0300

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,7 +26,7 @@ function About(props) {
               software development. So after that I signed up for a coding
               bootcamp, where I worked on several projects. I have some
               experience with the English language, which opens many doors for
-              me.  am currently looking for a job in the IT industry.
+              me. I am currently looking for a job in the IT industry.
               <Col className={styles.countryContainer}>
                 Country:{" "}
                 <Image
